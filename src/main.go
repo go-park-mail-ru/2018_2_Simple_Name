16e668660e9c001efab89994e107108a5f5c1c64
Kirill Sonk <ikirsonk@gmail.com>
2018-11-25 16:42:38 +0300

    cors url changed

--- src/main.go
+++ src/main.go
@@ -36,7 +36,7 @@ import (
 func CORSsettings(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "http://95.163.209.195:443")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Max-Age", "86400")
 		w.Header().Set("Access-Control-Allow-Headers",
